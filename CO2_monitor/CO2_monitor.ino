327d5bfc1aad015377283e6a718cad9b892ed435
Z. Paul Weleschuk <59177705+ZPaulWeleschuk@users.noreply.github.com>
2023-11-11 09:24:08 -0700

    clean up code

--- CO2_monitor/CO2_monitor.ino
+++ CO2_monitor/CO2_monitor.ino
@@ -1,5 +1,4 @@
 
-#define SenseAir_S8 // SenseAir S8
 // UNCOMMENT FOR DEBUG MODE
 //#define DEBUG
 
@@ -13,7 +12,6 @@
 #include <CRCx.h> //https://github.com/hideakitai/CRCx
 #include <avr/wdt.h>
 
-const byte RevVersion = 147; // Firmware version 23 aug 2021
 
 const byte PIN_TX = 6;  // define TX pin to Sensor
 const byte PIN_RX = 7;  // define RX pin to Sensor
@@ -21,18 +19,12 @@ const byte PIN_CLK = 9;
 const byte PIN_DIO = 8;
 
 const byte BUTTON_CALI = A0;
-const byte BUTTON_BEEP = A3;
-const byte BUTTON_ALTI = 2;
-const byte BUZZER = 11;
-const byte LED = 13;
 
-const byte addressBEEP = 0; // EEPROM address 0
-const byte addressALTI = 1; // EEPROM address 1
+const byte LED = 13;
 
 
 const byte MB_PKT_7 = 7;   //MODBUS Packet Size
 const byte MB_PKT_8 = 8;   //MODBUS Packet Size
-
 const byte MB_PKT_17 = 17; // MODBUS Packet Size
 
 
@@ -48,16 +40,14 @@ const byte cmdCalR[MB_PKT_7] = {0xFE, 0x03, 0x02, 0x00, 0x20, 0xAD, 0x88};      
 static byte response[MB_PKT_8] = {0};
 
 bool isLongPressCALI = false;
-bool isLongPressBEEP = false;
-bool isLongPressALTI = false;
 bool ExitCali = false;
 
-byte VALbeep = 0;
-int VALDIS = 0;
-byte VALalti = 21;//TODO:calgary is 1045m elevation, as 1045/50 = 20.9
-//int VALalti = 21;//TODO:calgary is 1045m elevation, as 1045/50 = 20.9
-//TODO:we are having issue where alti is being set to max byte (ie 255) thus when times by 50 results in 12750m elevation. we need to figure this out
-//TODO:perhaps we should write this as a float//TODO:
+
+
+//VALalti is altitude in meters divided by 50
+//therefore Calgary is 1045m elevation, as 1045/50 = 20.9
+byte VALalti = 21;
+
 byte ConnRetry = 0;
 int CO2 = 0;
 int CO2value;
@@ -68,67 +58,42 @@ unsigned long StartPress_ms = 0;
 float CO2cor;
 float hpa;
 
+
 const int LongPress_ms = 5000; // 5s button timeout
 
 
 SevenSegmentExtended display(PIN_CLK, PIN_DIO);
 SoftwareSerial co2sensor(PIN_RX, PIN_TX);
 
-
-#define BAUDRATE 9600 // Device to SenseAir S8, MHZ19 and CM1106 Serial baudrate (should not be changed)
+#define BAUDRATE 9600 // Device to SenseAir S8
 
 
 void setup()
 {
   pinMode(BUTTON_CALI, INPUT_PULLUP);
-  pinMode(BUTTON_BEEP, INPUT_PULLUP);
-  pinMode(BUTTON_ALTI, INPUT_PULLUP);
-  pinMode(BUZZER, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A5, OUTPUT);
   pinMode(LED, OUTPUT);
+    pinMode(A2, OUTPUT);
 
-  digitalWrite(BUZZER, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(A2, LOW);
-  digitalWrite(A5, LOW);
+    digitalWrite(A2, LOW);//for calibration button
   digitalWrite(LED, LOW);
 
   Serial.begin(9600);
 
   display.begin();           // initializes the display
-  display.setBacklight(100); // set the brightness to 100 %
+  display.setBacklight(50); // set the brightness in percent (ie: 100 => 100 %)
   delay(100);
 
-  Serial.print(F("Firmware version: "));
-  Serial.println(RevVersion);
-
   display.clear();
-  display.print("Lco2");//TODO:remove
-  Serial.println(F("Start LibreCO2"));//TODO:rename
+  display.print("co2");
 
-#ifdef SenseAir_S8
   Serial.println(F("Start SenseAir S8 lecture"));
-#endif
   co2sensor.begin(BAUDRATE); // Sensor serial start
   delay(4000);
 
-  VALbeep = EEPROM.read(addressBEEP);
-  //displayVALbeep(1000);
-  delay(1500);
 
-  //VALalti = EEPROM.read(addressALTI);//TODO:do we need to save alti to the eeprom??. seems overly complex
-  //displayVALalti();
-  delay(1500);
-
-
-
-#ifdef SenseAir_S8
   CO2iniSenseAir();
-#endif
 
-  // Preheat routine: min 30 seconds for SCD30 & CM1106 & SenseAir S8, 180 seconds for MH-Z14 or 19
+  // Preheat routine: min 30 seconds for  SenseAir S8
   Serial.print(F("Preheat: "));
 
   for (int i = 30; i > -1; i--)
@@ -147,15 +112,12 @@ void setup()
   Serial.println(" m");
   display.clear();
   display.print("CO2-");
-  delay(5000);
+  delay(3000);
 }
 
 void loop()
 {
   CO2 = 0;
-
-
-
 
   CO2value = co2SenseAir();
   if (CO2value != 0)
@@ -226,13 +188,7 @@ void BadConn()
   ConnRetry++;
 }
 
-// Beep 900Hhz
-void Beep()
-{
-  tone(BUZZER, 900);
-  delay(330);
-  noTone(BUZZER);
-}
+
 
 void Done()
 {
@@ -290,15 +246,10 @@ void CheckResponse(uint8_t *a, uint8_t *b, uint8_t len_array_cmp)
 
 void hPaCalculation()
 {
-  //hpa = 1013 - 5.9 * float(VALalti) + 0.011825 * float(VALalti) * float(VALalti); // Cuadratic regresion formula obtained PA (hpa) from high above the sea
-  hpa = 1019.1;//we can pull kpa from BME so no need to calc. 
-  //TODO:typically reading 1050ish units of co2 in office. need to take outside and see what reading is, should be about 412ppm
-  //or why dont we define it as this?. . . //TODO://TODO:
+  hpa = 1013 - 5.9 * float(VALalti) + 0.011825 * float(VALalti) * float(VALalti); // quadratic regresion formula obtained PA (hpa) from high above the sea
   Serial.print(F("Atmospheric pressure calculated by the sea level inserted (hPa): "));
   Serial.println(hpa);
 }
-
-
 
 
 
@@ -327,9 +278,8 @@ void CO2iniSenseAir()
   display.clear();
   delay(10);
   display.print("good");
-  //delay(2500);
   Serial.println(F("SenseAir read OK"));
-  delay(4000);
+  delay(3000);
 }
 
 // CO2 lecture SenseAir S8
@@ -341,20 +291,12 @@ int co2SenseAir()
   co2sensor.readBytes(responseSA, MB_PKT_7);
   CO2 = (256 * responseSA[3]) + responseSA[4];
 
-#ifdef DEBUG
-  Serial.print(F("Read SenseAir S8: "));
-#endif
 
   if (CO2 != 0)
   {
     crc_cmd = crcx::crc16(responseSA, 5);
     if (responseSA[5] == lowByte(crc_cmd) && responseSA[6] == highByte(crc_cmd))
     {
-#ifdef DEBUG
-      Serial.println(F("OK DATA"));
-#else
-      Serial.print("OK");
-#endif
       return CO2;
     }
     else
@@ -362,11 +304,7 @@ int co2SenseAir()
       while (co2sensor.available() > 0)
         char t = co2sensor.read(); // Clear serial input buffer;
 
-#ifdef DEBUG
-      Serial.println(F("FAIL CRC_CO2"));
-#else
-      Serial.println(F("FAIL"));
-#endif
+
       display.clear();
       display.print("----");
       return 0;
@@ -374,11 +312,7 @@ int co2SenseAir()
   }
   else
   {
-#ifdef DEBUG
-    Serial.println(F("FAILED"));
-#else
-    Serial.println(F("FAIL"));
-#endif
+
     display.clear();
     delay(10);
     display.print("----");
